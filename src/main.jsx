b6631d72843da3a994f5c301569f2be793783237
DHANANI YASHKUMAR <127470299+yashdhanani@users.noreply.github.com>
2025-07-01 09:08:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import 'uno.css'; // ✅ REQUIRED to apply UnoCSS
+import 'virtual:uno.css';
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
